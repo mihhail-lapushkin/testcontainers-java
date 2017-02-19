0c38011315e4baad193d89a5e450df61830f384a
Richard North <rich.north@gmail.com>
2017-02-19 22:19:45 +0000

    Fix test timing issue

--- core/src/test/java/org/testcontainers/utility/DirectoryTarResourceTest.java
+++ core/src/test/java/org/testcontainers/utility/DirectoryTarResourceTest.java
@@ -1,38 +1,44 @@
 package org.testcontainers.utility;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.output.ToStringConsumer;
+import org.testcontainers.containers.output.WaitingConsumer;
 import org.testcontainers.containers.startupcheck.OneShotStartupCheckStrategy;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
 
+//@Slf4j
 public class DirectoryTarResourceTest {
-
-    @Rule
-    public GenericContainer container = new GenericContainer(
-            new ImageFromDockerfile()
-                    .withDockerfileFromBuilder(builder ->
-                            builder.from("alpine:3.3")
-                                    .copy("/tmp/foo", "/foo")
-                                    .cmd("cat /foo/src/test/resources/test-recursive-file.txt")
-                                    .build()
-                    ).withFileFromFile("/tmp/foo", new File(".")))
-            .withStartupCheckStrategy(new OneShotStartupCheckStrategy());
-
 
     @Test
     public void simpleTest() throws TimeoutException {
 
+        WaitingConsumer wait = new WaitingConsumer();
+
         final ToStringConsumer toString = new ToStringConsumer();
-        container.followOutput(toString);
+
+        GenericContainer container = new GenericContainer(
+                new ImageFromDockerfile()
+                        .withDockerfileFromBuilder(builder ->
+                                builder.from("alpine:3.3")
+                                        .copy("/tmp/foo", "/foo")
+                                        .cmd("cat /foo/src/test/resources/test-recursive-file.txt")
+                                        .build()
+                        ).withFileFromFile("/tmp/foo", new File(".")))
+                .withStartupCheckStrategy(new OneShotStartupCheckStrategy())
+                .withLogConsumer(wait.andThen(toString));
+
+        container.start();
+        wait.waitUntilEnd(60, TimeUnit.SECONDS);
 
         final String results = toString.toUtf8String();
+//        log.info(results);
 
         assertTrue("The container has a file that was copied in via a recursive copy", results.contains("Used for DirectoryTarResourceTest"));
     }
